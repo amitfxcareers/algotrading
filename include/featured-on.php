6e003d3079cd72d1788330d54b16dc9d03ee387a
amitfxcareers <amit@fxcareers.com>
2024-07-16 17:25:09 +0530

    Featured on section created

---
+++ include/featured-on.php
@@ -0,0 +1,21 @@
+<section class="featured-on">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="featured-websites">
+					<h2 class="subheading mb-4">
+						AS FEATURED ON
+					</h2>
+					<div class="featured-web">
+						<a href="#">
+							<img src="assets/images/alpaca.svg">
+						</a>
+						<a href="#">
+							<img src="assets/images/ninjatrader.svg">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include_once('./include/header.php') ?>
 
+<?php include_once('./include/featured-on.php') ?>
 
 <?php include_once('./include/trading-excellence.php') ?>
 <?php include_once('./include/footer.php') ?>
