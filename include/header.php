9a4e479530a286a7599231303eec2e0282a0bda3
amitfxcareers <amit@fxcareers.com>
2024-07-16 16:47:45 +0530

    trading excellence section created and added on all pages

--- include/header.php
+++ include/header.php
@@ -3,7 +3,7 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Start Your Own Forex Brokerage | Forex Broker Services</title>
+      <title>Create and Execute Simple to Complex Algo Trading Strategies with Ease</title>
       <link rel="shortcut icon" href="./images/favicon.webp" type="image/x-icon">
       <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
       <link rel="stylesheet" type="text/css" href="assets/css/style.css">
---
+++ include/trading-excellence.php
@@ -0,0 +1,22 @@
+<section class="trading-excellence">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h2 class="mainheading">
+					Are you ready to take the first step towards trading excellence?
+				</h2>
+				<p>
+					Transform your trading approach and achieve the success you've always envisioned with AlgoTrading.
+				</p>
+				<div class="cta-button">
+					<a href="#" class="cta_one">
+						Get Started NOW !
+					</a>
+					<a href="#" class="cta_two">
+						Request a Demo
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
