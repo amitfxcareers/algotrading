29873d4f740cfb04e0c23609ac92fd4cf3a5f318
amitfxcareers <amit@fxcareers.com>
2024-07-17 12:44:34 +0530

    THE PROBLEM Section done

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                     </h1>
                     <p class="primary-para">
                         Is your algo trading idea complex and you want to create your own unique algo trading journey?
-                        AlgoBulls allows your trading ideas to run wild and is able to easily accommodate all your
+                        AlgoTrading allows your trading ideas to run wild and is able to easily accommodate all your
                         requirements with unparalleled algo trading solutions for everyone.
                     </p>
                     <div class="cta-button">
@@ -61,6 +61,31 @@
 			</div>
 			<div class="col-md-5 offset-md-1">
 				<img src="https://algobulls.com/img/python-vision.svg" class="img-fluid">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="the-problem-section">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-8">
+				<h4 class="subheading">
+					THE PROBLEM
+				</h4>
+				<h2 class="mainheading">
+					In Algo Trading, the majority of the time goes into tech and data management, leaving less for winning strategies.
+				</h2>
+			</div>
+			<div class="col-md-4">
+				<img src="https://algobulls.com/img/build-problem-main.svg" class="img-fluid">
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-12">
+				<h3 class="text-center">
+					At AlgoTrading, we understand this pain intimately. It's time to shift the paradigm, where technology works for you, not against you. Let's elevate your trading experience together with AlgoTrading
+				</h3>
 			</div>
 		</div>
 	</div>
