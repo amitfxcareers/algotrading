fadf1725ed7f2d0577854ca4074bb8be3d8b30ef
amitfxcareers <amit@fxcareers.com>
2024-07-18 12:49:32 +0530

    Extra space removed

--- index.php
+++ index.php
@@ -25,111 +25,111 @@
             </div>
             <div class="col-md-6">
                 <div class="hero-empowerment-right-content">
-                        <div class="top-content">
-                            <img class="img-fluid mobile" src="https://wpriverthemes.com/HTML/synck/assets/imgs/bg1.png" alt="Empowerment">
-
-                            <div class="experience-box simple-shadow bounce-in">
-                                <div class="experience-body d-flex align-items-center">
-                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/icon1.svg" alt="Icon" class="img-fluid">
-
-                                    <div class="experience-content d-flex align-items-center">
-                                        <h2>+8</h2>
-                                        <p>
-                                            Years
-                                            <span>Experience</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="bottom-content d-flex">
-                            <a href="./team.html" class="our-expert-team-box simple-shadow bounce-in delay-2">
-                                <div class="our-expert-team-box-inner d-flex align-items-center">
-                                    <div class="imgs imgs1 d-flex align-items-center">
-                                        <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
-                                        <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
-                                        <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
-                                        <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
-                                    </div>
+                    <div class="top-content">
+                        <img class="img-fluid mobile" src="https://wpriverthemes.com/HTML/synck/assets/imgs/bg1.png" alt="Empowerment">
+                        <div class="experience-box simple-shadow bounce-in">
+                            <div class="experience-body d-flex align-items-center">
+                                <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/icon1.svg" alt="Icon" class="img-fluid">
+                                <div class="experience-content d-flex align-items-center">
+                                    <h2>+8</h2>
                                     <p>
-                                        Meet
-                                        <span>Our Experts</span>
-                                    </p>
-                                </div>
-                            </a>
-
-                            <div class="google-reviews-box simple-shadow bounce-in delay-3">
-                                <div class="left">
-                                    <span>Verified by</span>
-                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/icon2.svg" alt="Google" class="img-fluid">
-                                </div>
-                                <div class="right">
-                                    <div class="stars d-flex align-items-center">
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                    </div>
-                                    <p>
-                                        3245
-                                        <span>Reviews</span>
+                                        Years
+                                        <span>Experience</span>
                                     </p>
                                 </div>
                             </div>
                         </div>
-
                     </div>
+                    <div class="bottom-content d-flex">
+                        <a href="./team.html" class="our-expert-team-box simple-shadow bounce-in delay-2">
+                            <div class="our-expert-team-box-inner d-flex align-items-center">
+                                <div class="imgs imgs1 d-flex align-items-center">
+                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
+                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
+                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
+                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
+                                </div>
+                                <p>
+                                    Meet
+                                    <span>Our Experts</span>
+                                </p>
+                            </div>
+                        </a>
+                        <div class="google-reviews-box simple-shadow bounce-in delay-3">
+                            <div class="left">
+                                <span>Verified by</span>
+                                <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/icon2.svg" alt="Google" class="img-fluid">
+                            </div>
+                            <div class="right">
+                                <div class="stars d-flex align-items-center">
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                </div>
+                                <p>
+                                    3245
+                                    <span>Reviews</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </section>
 <?php include_once('./include/featured-on.php') ?>
-
+<!-- OUR VISION -->
 <section class="our-vision" style="background-image: url(https://algobulls.com/img/inner-hero-build.svg);">
-	<div class="container">
-		<div class="row align-items-center gy-4">
-			<div class="col-md-6">
-				<h4 class="subheading">
-					OUR VISION
-				</h4>
-				<h2 class="mainheading">
-					To provide the most advanced and user friendly algo trading solution to a wide spectrum of traders.
-				</h2>
-			</div>
-			<div class="col-md-5 offset-md-1">
-				<img src="https://algobulls.com/img/python-vision.svg" class="img-fluid">
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row align-items-center gy-4">
+            <div class="col-md-6">
+                <h4 class="subheading">
+                    OUR VISION
+                </h4>
+                <h2 class="mainheading">
+                    To provide the most advanced and user friendly algo trading solution to a wide spectrum of traders.
+                </h2>
+            </div>
+            <div class="col-md-5 offset-md-1">
+                <img src="https://algobulls.com/img/python-vision.svg" class="img-fluid">
+            </div>
+        </div>
+    </div>
 </section>
+<!-- OUR VISION -->
 
+<!-- THE PROBLEM -->
 <section class="the-problem-section">
-	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-md-8">
-				<h4 class="subheading">
-					THE PROBLEM
-				</h4>
-				<h2 class="mainheading">
-					In Algo Trading, the majority of the time goes into tech and data management, leaving less for winning strategies.
-				</h2>
-			</div>
-			<div class="col-md-4">
-				<img src="https://algobulls.com/img/build-problem-main.svg" class="img-fluid">
-			</div>
-		</div>
-		<div class="row mt-5">
-			<div class="col-md-12">
-				<h3 class="text-center">
-					At AlgoTrading, we understand this pain intimately. It's time to shift the paradigm, where technology works for you, not against you. Let's elevate your trading experience together with AlgoTrading
-				</h3>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-8">
+                <h4 class="subheading">
+                    THE PROBLEM
+                </h4>
+                <h2 class="mainheading">
+                    In Algo Trading, the majority of the time goes into tech and data management, leaving less for
+                    winning strategies.
+                </h2>
+            </div>
+            <div class="col-md-4">
+                <img src="https://algobulls.com/img/build-problem-main.svg" class="img-fluid">
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <h3 class="text-center">
+                    At AlgoTrading, we understand this pain intimately. It's time to shift the paradigm, where
+                    technology works for you, not against you. Let's elevate your trading experience together with
+                    AlgoTrading
+                </h3>
+            </div>
+        </div>
+    </div>
 </section>
-
+<!-- THE PROBLEM -->
 
 
 <?php include_once('./include/trading-excellence.php') ?>
