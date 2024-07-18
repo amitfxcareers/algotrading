59f41e46a70236396e20a968968f0a2046c037cd
amitfxcareers <amit@fxcareers.com>
2024-07-18 15:57:18 +0530

    OUR SOLUTION Section done

--- index.php
+++ index.php
@@ -26,10 +26,12 @@
             <div class="col-md-6">
                 <div class="hero-empowerment-right-content">
                     <div class="top-content">
-                        <img class="img-fluid mobile" src="https://wpriverthemes.com/HTML/synck/assets/imgs/bg1.png" alt="Empowerment">
+                        <img class="img-fluid mobile" src="https://wpriverthemes.com/HTML/synck/assets/imgs/bg1.png"
+                            alt="Empowerment">
                         <div class="experience-box simple-shadow bounce-in">
                             <div class="experience-body d-flex align-items-center">
-                                <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/icon1.svg" alt="Icon" class="img-fluid">
+                                <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/icon1.svg" alt="Icon"
+                                    class="img-fluid">
                                 <div class="experience-content d-flex align-items-center">
                                     <h2>+8</h2>
                                     <p>
@@ -44,10 +46,14 @@
                         <a href="./team.html" class="our-expert-team-box simple-shadow bounce-in delay-2">
                             <div class="our-expert-team-box-inner d-flex align-items-center">
                                 <div class="imgs imgs1 d-flex align-items-center">
-                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
-                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
-                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
-                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png" alt="team" class="img-fluid">
+                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png"
+                                        alt="team" class="img-fluid">
+                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png"
+                                        alt="team" class="img-fluid">
+                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png"
+                                        alt="team" class="img-fluid">
+                                    <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/small-img-4.png"
+                                        alt="team" class="img-fluid">
                                 </div>
                                 <p>
                                     Meet
@@ -58,7 +64,8 @@
                         <div class="google-reviews-box simple-shadow bounce-in delay-3">
                             <div class="left">
                                 <span>Verified by</span>
-                                <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/icon2.svg" alt="Google" class="img-fluid">
+                                <img src="https://wpriverthemes.com/HTML/synck/assets/imgs/icon2.svg" alt="Google"
+                                    class="img-fluid">
                             </div>
                             <div class="right">
                                 <div class="stars d-flex align-items-center">
@@ -130,7 +137,135 @@
     </div>
 </section>
 <!-- THE PROBLEM -->
-
+<!-- OUR SOLUTION -->
+<section class="our-solution-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h4 class="subheading">
+                    THE PROBLEM
+                </h4>
+                <h2 class="mainheading">
+                    AlgoTrading - the algo trading platform that is meant for everyone. From beginners to advanced
+                    traders and from retail to enterprise traders.
+                </h2>
+                <p class="primary-para">
+                    AlgoTrading is your revolutionary solution that addresses each algo trading pain point with
+                    precision.
+                </p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="our_solutions_list text-center">
+                    <ul class="nav nav-pills mb-3 list_solutions" id="pills-tab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="pills-one-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-one" type="button" role="tab" aria-controls="pills-one" aria-selected="true">Any Platform</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="pills-two-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-two" type="button" role="tab" aria-controls="pills-two" aria-selected="false">Any Cloud or On-premise</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="pills-three-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-three" type="button" role="tab" aria-controls="pills-three"
+                                aria-selected="false">Market Versatility</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="pills-four-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-four" type="button" role="tab" aria-controls="pills-four"
+                                aria-selected="false">Generative AI</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="pills-five-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-five" type="button" role="tab" aria-controls="pills-five"
+                                aria-selected="false">Ready Strategy library</button>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-one" role="tabpanel"
+                            aria-labelledby="pills-one-tab">
+                                <div class="container">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-6">
+                                            <p>
+                                                Experience flexibility like never before. AlgoBulls adapts to your preferred platform – whether it's a web app, mobile apps, Jupyter notebooks, or CLIs.
+                                            </p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <img src="https://algobulls.com/img/python-vision.svg" class="img-fluid">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <div class="tab-pane fade" id="pills-two" role="tabpanel"
+                            aria-labelledby="pills-two-tab">
+                                <div class="container">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-6">
+                                            <img src="https://algobulls.com/img/python-vision.svg" class="img-fluid">
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>
+                                                Enjoy the freedom of choice. AlgoBulls Cloud, your cloud, or on-premise – deploy AlgoBulls’ solutions wherever you want, ensuring 100% IP protection at all times.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <div class="tab-pane fade" id="pills-three" role="tabpanel"
+                            aria-labelledby="pills-three-tab">
+                                <div class="container">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-6">
+                                            <p>
+                                                Break free from limitations. Algo Trade seamlessly on Equities, F&O, Commodity, Currency across NASDAQ, NYSE, NSE, BSE and more with the broker of your choice.
+                                            </p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <img src="https://algobulls.com/img/python-vision.svg" class="img-fluid">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="pills-four" role="tabpanel"
+                            aria-labelledby="pills-four-tab">
+                                <div class="container">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-6">
+                                            <img src="https://algobulls.com/img/python-vision.svg" class="img-fluid">
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>
+                                                Unleash the power of generative AI. Translate your algo trading ideas into reality with ease by generating strategy codes and parameters through simple prompts in plain English.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="pills-five" role="tabpanel"
+                            aria-labelledby="pills-five-tab">
+                                <div class="container">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-6">
+                                            <p>
+                                                Leverage a library of tested and proven strategy codes, saving you time and providing a foundation for successful algo trading strategies.
+                                            </p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <img src="https://algobulls.com/img/python-vision.svg" class="img-fluid">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- OUR SOLUTION -->
 
 <?php include_once('./include/trading-excellence.php') ?>
 <?php include_once('./include/footer.php') ?>
